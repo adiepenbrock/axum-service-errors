5ec8d14c671efa61c06e01323df591bbcf09502f
Andreas Diepenbrock <mail@adiepenbrock.de>
2025-07-03 10:10:38 +0200

    feat: add pluggable response builders with JSON feature flag
    - Add ResponseBuilder trait for custom response formatting
    - Implement JsonResponseBuilder behind 'json' feature flag
    - Add PlainTextResponseBuilder as default response format
    - Make serde_json optional dependency with feature flag
    - Change parameters from serde_json::Value to String for simplicity
    - Update ServiceError to support custom response builders
    - Remove axum json feature dependency from main dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,19 @@ use std::borrow::Cow;
 use std::collections::HashMap;
 
 use axum::{
-    Json,
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 use serde::{Serialize, Deserialize};
 
+/// A trait for building custom response formats from ServiceError data.
+pub trait ResponseBuilder: std::fmt::Debug {
+    /// Build a response body and content-type from the error data.
+    fn build(&self, error: &ServiceError) -> (String, &'static str);
+}
+
 /// A `ServiceError` represents a specific error within the software.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ServiceError<'a> {
     /// An internal error code that represents a specific error within the
     /// system.
@@ -28,7 +33,24 @@ pub struct ServiceError<'a> {
     pub arguments: Vec<String>,
     /// Optional parameters as key-value pairs
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub parameters: Option<HashMap<String, serde_json::Value>>,
+    pub parameters: Option<HashMap<String, String>>,
+    /// Custom response builder for formatting output
+    #[serde(skip)]
+    response_builder: Option<Box<dyn ResponseBuilder>>,
+}
+
+impl<'a> Clone for ServiceError<'a> {
+    fn clone(&self) -> Self {
+        Self {
+            code: self.code,
+            name: self.name.clone(),
+            http_status: self.http_status,
+            message: self.message.clone(),
+            arguments: self.arguments.clone(),
+            parameters: self.parameters.clone(),
+            response_builder: None, // Cannot clone trait objects
+        }
+    }
 }
 
 impl<'a> ServiceError<'a> {
@@ -41,6 +63,7 @@ impl<'a> ServiceError<'a> {
             message: Cow::Borrowed(message),
             arguments: Vec::new(),
             parameters: None,
+            response_builder: None,
         }
     }
 
@@ -51,11 +74,15 @@ impl<'a> ServiceError<'a> {
     }
 
     /// Add an optional parameter.
-    pub fn parameter(mut self, key: impl ToString, value: impl Serialize) -> Self {
+    pub fn parameter(mut self, key: impl ToString, value: impl ToString) -> Self {
         let parameters = self.parameters.get_or_insert_with(HashMap::new);
-        if let Ok(json_value) = serde_json::to_value(value) {
-            parameters.insert(key.to_string(), json_value);
-        }
+        parameters.insert(key.to_string(), value.to_string());
+        self
+    }
+
+    /// Set a custom response builder for formatting the response.
+    pub fn with_response_builder(mut self, builder: impl ResponseBuilder + 'static) -> Self {
+        self.response_builder = Some(Box::new(builder));
         self
     }
 
@@ -72,28 +99,108 @@ impl<'a> ServiceError<'a> {
 
 impl<'a> IntoResponse for ServiceError<'a> {
     fn into_response(self) -> Response {
-        // try to convert the given status code to a `StatusCode` instance, if it fails,
-        // we will default to `INTERNAL_SERVER_ERROR`.
         let status_code =
             StatusCode::from_u16(self.http_status).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
 
-        let response_body = ResponseBody {
-            code: self.code,
-            name: self.name.clone(),
-            message: self.format_message(),
-            parameters: self.parameters.clone(),
+        let (body, content_type) = if let Some(builder) = &self.response_builder {
+            builder.build(&self)
+        } else {
+            // Default plain text format
+            let text = if let Some(ref params) = self.parameters {
+                format!(
+                    "Error {}: {} - {} (Parameters: {:?})",
+                    self.code,
+                    self.name,
+                    self.format_message(),
+                    params
+                )
+            } else {
+                format!(
+                    "Error {}: {} - {}",
+                    self.code,
+                    self.name,
+                    self.format_message()
+                )
+            };
+            (text, "text/plain")
         };
 
-        (status_code, Json(response_body)).into_response()
+        (
+            status_code,
+            [("content-type", content_type)],
+            body,
+        ).into_response()
     }
 }
 
+/// A simple JSON response builder that serializes the ServiceError as JSON.
+#[cfg(feature = "json")]
+#[derive(Debug, Clone)]
+pub struct JsonResponseBuilder;
+
+#[cfg(feature = "json")]
+impl JsonResponseBuilder {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+#[cfg(feature = "json")]
+impl ResponseBuilder for JsonResponseBuilder {
+    fn build(&self, error: &ServiceError) -> (String, &'static str) {
+        let response_body = JsonResponseBody {
+            code: error.code,
+            name: error.name.clone(),
+            message: error.format_message(),
+            parameters: error.parameters.clone(),
+        };
+        
+        let json = serde_json::to_string(&response_body)
+            .unwrap_or_else(|_| format!("{{\"error\":\"Failed to serialize error {}\"}}", error.code));
+        
+        (json, "application/json")
+    }
+}
+
+#[cfg(feature = "json")]
 #[derive(Debug, Clone, Serialize)]
-struct ResponseBody<'a> {
+struct JsonResponseBody<'a> {
     code: u32,
     #[serde(borrow)]
     name: Cow<'a, str>,
     message: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    parameters: Option<HashMap<String, serde_json::Value>>,
+    parameters: Option<HashMap<String, String>>,
+}
+
+/// A simple plain text response builder.
+#[derive(Debug, Clone)]
+pub struct PlainTextResponseBuilder;
+
+impl PlainTextResponseBuilder {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl ResponseBuilder for PlainTextResponseBuilder {
+    fn build(&self, error: &ServiceError) -> (String, &'static str) {
+        let text = if let Some(ref params) = error.parameters {
+            format!(
+                "Error {}: {} - {} (Parameters: {:?})",
+                error.code,
+                error.name,
+                error.format_message(),
+                params
+            )
+        } else {
+            format!(
+                "Error {}: {} - {}",
+                error.code,
+                error.name,
+                error.format_message()
+            )
+        };
+        (text, "text/plain")
+    }
 }
